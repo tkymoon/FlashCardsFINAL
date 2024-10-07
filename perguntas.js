8371279e5abda7d908d1511d1e630874855aaeeb
Maria Pavanelo <144125190+tkymoon@users.noreply.github.com>
2024-10-07 07:27:14 -0300

    Parte Final

---
+++ perguntas.js
@@ -0,0 +1,24 @@
+criaCartao(
+    '<span style="color: rgb(255, 232, 182);">ESTRELAS</span>',
+    'O que varia a temperatura delas?',
+    ' A temperaura de uma estrela varia de acordo com sua <span style="color: rgb(255, 255, 185);">massa e idade.</span>Umas têm temperaturas mais altas porque a pressão em seus núcleos é maior, acelerando as reações de fusão nuclear.Estrelas jovens também tendem a ser mais quentes, enquanto as mais velhas, são mais frias. A cor delas indica sua temperatura:estrelas azuis são quentes, enquantos as vermelhas são frias.'
+)
+
+criaCartao(
+    '<span style="color: rgb(255, 127, 110);">BURACOS NEGROS</span>',
+    'Como são explicados?',
+    'São regiões onde a gravidade é tão imensa que nada, nem mesmo luz,pode escapar.Eles se formam quando uma estrela muito massiva explode em uma supernova e seu núcleo colapsa, comprimindo-se em um ponto extremamente denso chamado <span style="color: rgb(255, 72, 0);">"singularidade"</span>.Ao redor dela, há uma área chamada <span style="color: rgb(255, 72, 0);">"horizonte de eventos"</span>, que é a fronteira além da qual nada pode voltar, quando se chega perto o suficiente.</p>'
+
+)
+
+criaCartao(
+    '<span style="color: rgb(118, 202, 128);">PLANETAS</span>',
+    'Como funcionam as órbitas?',
+    'As órbitas dos planetas funcionam graças a um <span style="color: rgb(94, 214, 110);">equilíbrio entre a gravidade</span> do Sol e a <span style="color: rgb(94, 214, 110);">velocidade</span> dos planetas.A gravidade do Sol puxa os planetas em sua direção, mas como eles estão se movendo rapidamente, em vez de serem atraídos diretamente,eles entram em uma trajetória curva ao redor da estrela. Esse movimento contínuo faz com que os planetas fiquem presos em órbitas elípticas (levemente ovaladas).'
+)
+
+criaCartao(
+    '<span style="color: rgb(140, 161, 255);">NEBULOSAS</span>',
+    'Como surgem e o que caracterizam seus formatos?',
+    'As nebulosas surgem da <span style="color: rgb(140, 161, 255);">acumulação de gás e poeira</span>, muitas vezes após explosões de estrelas.<br> Suas formas são definidas pela <span style="color: rgb(140, 161, 255);"> gravidade, rotação e a influência de estrelas próximas</span>.Por <br>exemplo, nebulosas de emissão apresentam formas irregulares devido à ionização do gás,<br> enquanto nebulosas planetárias,resultantes da expulsão de camadas externas de estrelas, <br>costumam ter formas mais esféricas. Estruturas filamentosas são frequentementeformadas <br>por choques e interações do gás com a radiação estelar, criando um visual intrigante no cosmos.'
+)
